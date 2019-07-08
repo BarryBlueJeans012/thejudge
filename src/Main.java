8cf043f91b313e6fed9fdbfb9dbc84c75d3a35e8
BarryBlueJeans012 <40247032+barrybluejeans012@users.noreply.github.com>
2019-07-08 12:41:23 -0700

    removed some comments

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,7 @@ public class Main
 
         System.out.println("Hell");
         System.out.println("o");
-        // this is from JEss ce
-        // an edit from J
+
         FileRunner execute = new FileRunner();
         execute.runFile("Main");
         System.out.println("Done");
