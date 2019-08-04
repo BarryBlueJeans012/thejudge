1b7edb8de08a8e941bb761c4c13b1f0f253e58ad
jessehenrick <Jesse.henrick012@gmail.com>
2019-08-03 19:40:38 -0700

    -jesse

--- src/file/FileRunWithProcessBuilder.java
+++ src/file/FileRunWithProcessBuilder.java
@@ -4,16 +4,33 @@ import java.io.File;
 
 public class FileRunWithProcessBuilder
 {
-    private static String className = null;
-    private static String home_var = System.getenv("HOME");
+    private String fileName = null;
+    private String className = null;
+    private boolean hasCompiled = false;
+    private String homeDirectory = System.getenv("HOME");
+    private String projectDirectory = homeDirectory + "/IdeaProjects/thejudge";
+    private String workingDirectory = homeDirectory + "/judge_boy";
+    private String outputFile = projectDirectory + "/src/test/output";
+    private String inputFile = projectDirectory + "/src/test/input";
+    private String errorFile = projectDirectory + "/src/test/error";
 
-    public static void compile(String fileName)
+    public FileRunWithProcessBuilder(String fileName)
     {
+        this.fileName = fileName;
+        className = fileName.replace(".java", "");
+        outputFile = outputFile.replace("output", className + "_output");
+        errorFile = errorFile.replace("error", className + "_error");
+    }
+
+    public void compile()
+    {
+        assert fileName != null;
         ProcessBuilder builder = new ProcessBuilder();
-        builder.command("javac", fileName);
-        //builder.inheritIO();
-        builder.redirectOutput(new File(home_var + "/IdeaProjects/thejudge/src/test/output.txt"));
-        builder.directory(new File(home_var + "/judge_boy"));
+        builder.command("javac", "-d", "out", fileName);
+        builder.inheritIO();
+        //builder.redirectOutput(new File(outputFile));
+        builder.redirectError(new File(errorFile));
+        builder.directory(new File(workingDirectory));
         try
         {
             Process process = builder.start();
@@ -21,26 +38,25 @@ public class FileRunWithProcessBuilder
             System.out.println("Compilation exited with code: " + exitCode);
             if (exitCode == 0)
             {
-                className = fileName.replace(".java", "");
+                hasCompiled = true;
             }
         }
         catch(Exception e)
         {
-            System.out.println("There was a fatal error");
+            System.out.println("There was a fatal error in compilation");
             e.printStackTrace();
         }
-
-
     }
 
-    public static void run()
+    public void run()
     {
-        assert className != null;
+        assert hasCompiled;
         ProcessBuilder builder = new ProcessBuilder();
         builder.command("java", className);
-        //builder.inheritIO();
-        builder.redirectOutput(new File(home_var + "/IdeaProjects/thejudge/src/test/output.txt"));
-        builder.directory(new File(home_var + "/judge_boy"));
+        builder.inheritIO();
+        builder.redirectOutput(new File(outputFile));
+        builder.redirectInput(new File(inputFile));
+        builder.directory(new File(workingDirectory + "/out"));
         try
         {
             Process process = builder.start();
@@ -49,16 +65,43 @@ public class FileRunWithProcessBuilder
         }
         catch(Exception e)
         {
-            System.out.println("There was a fatal error");
+            System.out.println("There was a fatal error in execution");
             e.printStackTrace();
         }
+    }
+
+    public void cleanOut()
+    {
+       File outDirectory = new File(workingDirectory + "/out");
+       File[] arrayOfClasses = outDirectory.listFiles();
+       int filesDeleted = 0;
+       if (arrayOfClasses != null)
+       {
+           for (File file : arrayOfClasses)
+           {
+                System.out.println("Deleting " + file.getAbsolutePath());
+                if (file.delete())
+                {
+                    System.out.println("Successfully deleted.");
+                }
+                else
+                {
+                    System.out.println("An error has occurred.");
+                }
+           }
+       }
+       System.out.println(filesDeleted + " files deleted.");
+
+
     }
 
 
 
     public static void main(String []args)
     {
-        compile("test_me.java");
-        run();
+        FileRunWithProcessBuilder runner = new FileRunWithProcessBuilder("test_me.java");
+        runner.compile();
+        runner.run();
+        runner.cleanOut();
     }
 }
